18b3d30bbec74e1cb3c3c6b2a83681bf76029c24
Zerossik <zeross19940303@gmail.com>
2023-03-12 17:14:32 +0200

    task one done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+function addGalleryEL() {
+  const result = galleryItems
+    .map(el => {
+      return `<a class="gallery__item" href="${el.original}">
+  <img class="gallery__image" src="${el.preview}" alt="${el.description}" />
+</a>`;
+    })
+    .join(' ');
+  galleryEl.insertAdjacentHTML('afterbegin', result);
+  return;
+}
+function handleImgClick(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  createModalWindow(evt);
+  return;
+}
+
+function createModalWindow() {
+  new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+}
+
+galleryEl.addEventListener('click', handleImgClick);
+addGalleryEL();
