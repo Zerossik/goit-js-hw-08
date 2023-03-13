3d831f23088c792927ad0c77c9fba3c4855db175
Zerossik <zeross19940303@gmail.com>
2023-03-13 19:46:42 +0200

    player added

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+// console.log(iframe);
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player.on('timeupdate', updateTime);
+function updateTime(data) {
+  console.log(data.seconds);
+}
