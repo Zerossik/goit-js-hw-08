f5f4cb9195034eb7a37d1931d38ee5c84cfe629c
Zerossik <zeross19940303@gmail.com>
2023-03-13 22:23:07 +0200

    task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,27 @@
+const formEl = document.querySelector('.feedback-form');
+console.log(formEl);
 
+const inputMessage = {};
+
+function handleInputValue(evt) {
+  inputMessage[evt.target.name] = evt.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(inputMessage));
+}
+
+function handleSubmitReset(evt) {
+  evt.preventDefault();
+  console.log(inputMessage);
+  formEl.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+getTextFromInput();
+
+function getTextFromInput() {
+  const localDate = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (localDate) {
+    // console.log(formEl.value);
+  }
+}
+
+formEl.addEventListener('input', handleInputValue);
+formEl.addEventListener('submit', handleSubmitReset);
